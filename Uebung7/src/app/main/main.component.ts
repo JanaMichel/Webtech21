cc072be132df42430f2e62b440fb9fca6c693a04
Michel <s0577677@htw-berlin.de>
2021-12-05 14:20:54 +0100

    Uebung7, entfernung von First projekt

---
+++ Uebung7/src/app/main/main.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import MembersJson from '../../assets/members.json';
+
+interface MEMBER {
+  forename: string;
+  surname: string;
+  email: string;
+}
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+  tableOn = true;
+  buttonText = 'Tabelle verstecken';
+  members: MEMBER[] = MembersJson;
+
+  show = true;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log(this.members);
+  }
+
+  tableOnOff() {
+    if(this.buttonText === 'Tabelle verstecken')
+    {
+      this.buttonText = 'Tabelle anzeigen'
+      this.show = false;
+    }
+    else{
+      this.buttonText = 'Tabelle verstecken'
+      this.show = true;
+    }
+    // diese Funktion muss implementiert werden
+    // buttonText aendern und
+    // tableOn umschalten
+  }
+}
+
