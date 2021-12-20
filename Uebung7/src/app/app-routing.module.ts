cd0760f5bd509da080ad5c53596047fcdc3bab0a
Michel <s0577677@htw-berlin.de>
2021-12-20 14:07:47 +0100

    SKript Uebung 9

--- Uebung7/src/app/app-routing.module.ts
+++ Uebung7/src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CitiesComponent } from './cities/cities.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: MainComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "cities",
+    component: CitiesComponent
+  }
+  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  providers: []
 })
 export class AppRoutingModule { }
+
--- Uebung7/src/app/app.module.ts
+++ Uebung7/src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { CitiesComponent } from './cities/cities.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent,
+    LoginComponent,
+    AboutComponent,
+    CitiesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
